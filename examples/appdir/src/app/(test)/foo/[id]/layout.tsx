1f9102e11f97c00911475bcb1f537cadaff6aef7
Ethan Niser <100045248+ethanniser@users.noreply.github.com>
2023-05-30 21:04:41 +0000

    sync to pc

---
+++ examples/appdir/src/app/(test)/foo/[id]/layout.tsx
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import {
+  withLayoutParamValidation,
+  type DynamicLayout,
+  type InferLayoutPropsType,
+} from "next-typesafe-url/app";
+
+const LayoutRoute = {
+  routeParams: z.object({
+    id: z.string(),
+  }),
+} satisfies DynamicLayout;
+type LayoutType = typeof LayoutRoute
+
+type Props = InferLayoutPropsType<LayoutType>
+function Layout({
+  children,
+  routeParams,
+}: Props) {
+  return (
+    <div>
+      <h1>THIS IS A LAYOUT</h1>
+      <p>{JSON.stringify(routeParams)}</p>
+      <div className="border border-black">{children}</div>
+      <p>bottom</p>
+    </div>
+  );
+}
+
+export default withLayoutParamValidation(Layout, LayoutRoute);
