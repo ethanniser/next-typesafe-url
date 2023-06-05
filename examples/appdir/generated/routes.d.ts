b11d299e4c0b1c8df451363b486897f5200ee558
Ethan Niser <thebestgamer2502@gmail.com>
2023-06-05 12:39:36 -0500

    example generated file change, idk how this didnt make the previous commit

--- examples/appdir/generated/routes.d.ts
+++ examples/appdir/generated/routes.d.ts
@@ -1,7 +1,7 @@
-import { type RouteType as Route_0 } from "../../../srcapp/(test)/foo/[id]/nest/routeType";
-import { type RouteType as Route_1 } from "../../../srcapp/(test)/foo/[id]/routeType";
-import { type RouteType as Route_2 } from "../../../srcapp/client/[...client]/routeType";
-import { type RouteType as Route_3 } from "../../../srcapp/[slug]/[...foo]/routeType";
+import { type RouteType as Route_0 } from "../../../examples/appdir/src/app/(test)/foo/[id]/nest/routeType";
+import { type RouteType as Route_1 } from "../../../examples/appdir/src/app/(test)/foo/[id]/routeType";
+import { type RouteType as Route_2 } from "../../../examples/appdir/src/app/client/[...client]/routeType";
+import { type RouteType as Route_3 } from "../../../examples/appdir/src/app/[slug]/[...foo]/routeType";
 import type { InferRoute, StaticRoute } from "next-typesafe-url";
 
 declare module "@@@next-typesafe-url" {
