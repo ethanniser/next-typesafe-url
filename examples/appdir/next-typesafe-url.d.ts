82d0e5f406e1c128e1c7fcf68824b74d49152e11
Ethan Niser <thebestgamer2502@gmail.com>
2023-06-06 12:10:09 -0500

    update examples

--- examples/appdir/next-typesafe-url.d.ts
+++ examples/appdir/next-typesafe-url.d.ts
@@ -10,6 +10,7 @@ import { type RouteType as Route_0 } from "./src/app/(test)/foo/[id]/nest/routeT
 import { type RouteType as Route_1 } from "./src/app/(test)/foo/[id]/routeType"
 import { type RouteType as Route_2 } from "./src/app/client/[...client]/routeType"
 import { type RouteType as Route_3 } from "./src/app/[slug]/[...foo]/routeType"
+import { type RouteType as Route_4 } from "./src/pages/dynamic"
 import type { InferRoute, StaticRoute } from "next-typesafe-url";
 
 declare module "@@@next-typesafe-url" {
@@ -18,9 +19,11 @@ declare module "@@@next-typesafe-url" {
     "/foo/[id]": InferRoute<Route_1>;
     "/client/[...client]": InferRoute<Route_2>;
     "/[slug]/[...foo]": InferRoute<Route_3>;
+    "/dynamic": InferRoute<Route_4>;
   }
 
   interface StaticRouter {
     "/": StaticRoute;
+    "/static": StaticRoute;
   }
 }
