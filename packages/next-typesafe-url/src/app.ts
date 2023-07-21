e41b0c3caebb7230dae416a2e12c4354143ea619
Ethan Niser <thebestgamer2502@gmail.com>
2023-07-21 06:51:51 -0500

    comment app hooks

--- packages/next-typesafe-url/src/app.ts
+++ packages/next-typesafe-url/src/app.ts
@@ -13,6 +13,11 @@ import {
   parseObjectFromStringRecord,
 } from "./utils";
 
+export {
+  withLayoutParamValidation,
+  withParamValidation,
+} from "./appComponents";
+
 function usePrevious<T>(value: T) {
   const ref = useRef<T>();
   // Store current value in ref
@@ -23,25 +28,35 @@ function usePrevious<T>(value: T) {
   return ref.current;
 }
 
-export {
-  withLayoutParamValidation,
-  withParamValidation,
-} from "./appComponents";
-
+/**
+ * Parses the current dynamic route params and validates them against the provided zod schema.
+ * Be careful if using this in a component that is used in multiple routes,
+ * making sure you pass the correct validator for the current route.
+ * @param validator - The zod schema to validate the params against, should come from your Route object
+ *
+ * @example
+ * const routeParams = useRouteParams(Route.routeParams);
+ * const { data, isLoading, isError, error } = routeParams;
+ */
 export function useRouteParams<T extends z.AnyZodObject>(
   validator: T
 ): UseParamsResult<T> {
   const params = useParams();
   const prev = usePrevious(params);
+  // to prevent infinite rerenders, we need to deep compare between renders
   const same = JSON.stringify(prev) === JSON.stringify(params);
   const [isError, setIsError] = useState(false);
+  // not used if theres no error, but we need to initialize it so just use a dummy error
   const [error, setError] = useState<z.ZodError>(new z.ZodError([]));
   const [data, setData] = useState<z.output<T> | undefined>(undefined);
 
   useEffect(() => {
+    // parse the params to a Record<string, unknown>
     const parsedRouteParams = parseObjectFromStringRecord(params);
+    // validate the params against the zod schema
     const validatedRouteParams = validator.safeParse(parsedRouteParams);
 
+    // update state based on the validation result
     if (validatedRouteParams.success) {
       setData(validatedRouteParams.data);
       setIsError(false);
@@ -50,9 +65,11 @@ export function useRouteParams<T extends z.AnyZodObject>(
       setIsError(true);
       setError(validatedRouteParams.error);
     }
+    // only rerun if the params have changed between renders
   }, [same]);
 
   if (isError) {
+    // if there was an error, return the error
     return {
       data: undefined,
       isError: true,
@@ -61,6 +78,7 @@ export function useRouteParams<T extends z.AnyZodObject>(
     };
   } else {
     if (!data) {
+      // if there was no error but the data is undefined, we're still loading
       return {
         data: undefined,
         isError: false,
@@ -68,6 +86,7 @@ export function useRouteParams<T extends z.AnyZodObject>(
         error: undefined,
       };
     } else {
+      // if there was no error and the data is defined, return the data
       return {
         data: data,
         isError: false,
@@ -78,18 +97,32 @@ export function useRouteParams<T extends z.AnyZodObject>(
   }
 }
 
+/**
+ * Parses the current dynamic route params and validates them against the provided zod schema.
+ * Be careful if using this in a component that is used in multiple routes,
+ * making sure you pass the correct validator for the current route.
+ * @param validator - The zod schema to validate the params against, should come from your Route object
+ *
+ * @example
+ * const searchParams = useSearchParams(Route.searchParams);
+ * const { data, isLoading, isError, error } = searchParams;
+ */
 export function useSearchParams<T extends z.AnyZodObject>(
   searchValidator: T
 ): UseParamsResult<T> {
   const params = useNextSearchParams();
   const [isError, setIsError] = useState(false);
+  // not used if theres no error, but we need to initialize it so just use a dummy error
   const [error, setError] = useState<z.ZodError>(new z.ZodError([]));
   const [data, setData] = useState<z.output<T> | undefined>(undefined);
 
   useEffect(() => {
+    // parse the params to a Record<string, unknown>
     const parsedSearchParams = parseObjectFromReadonlyURLParams(params);
+    // validate the params against the zod schema
     const validatedSearchParams = searchValidator.safeParse(parsedSearchParams);
 
+    // update state based on the validation result
     if (validatedSearchParams.success) {
       setData(validatedSearchParams.data);
       setIsError(false);
@@ -101,6 +134,7 @@ export function useSearchParams<T extends z.AnyZodObject>(
   }, [params]);
 
   if (isError) {
+    // if there was an error, return the error
     return {
       data: undefined,
       isError: true,
@@ -109,6 +143,7 @@ export function useSearchParams<T extends z.AnyZodObject>(
     };
   } else {
     if (!data) {
+      // if there was no error but the data is undefined, we're still loading
       return {
         data: undefined,
         isError: false,
@@ -116,6 +151,7 @@ export function useSearchParams<T extends z.AnyZodObject>(
         error: undefined,
       };
     } else {
+      // if there was no error and the data is defined, return the data
       return {
         data: data,
         isError: false,
