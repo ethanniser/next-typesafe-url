1c8847a3a47e0aecb3b432e0817afc4ba00850cc
Ethan Niser <ethan@niser.com>
2024-10-30 00:08:08 -0400

    oops

--- examples/appdir/src/app/[slug]/[...foo]/page.tsx
+++ examples/appdir/src/app/[slug]/[...foo]/page.tsx
@@ -2,25 +2,23 @@ import { withParamValidation } from "next-typesafe-url/app/hoc";
 import { InferPagePropsType } from "next-typesafe-url";
 import { Route, RouteType } from "./routeType";
 import { Client } from "./client";
+import { Suspense } from "react";
 
 type PageProps = InferPagePropsType<RouteType>;
 
-let count = 0;
-
 const Page = async ({ routeParams, searchParams }: PageProps) => {
-  console.log("render", count++);
   const params = await routeParams;
   const search = await searchParams;
   console.log(JSON.stringify(params));
   console.log(JSON.stringify(search));
 
   return (
-    <>
+    <Suspense>
       <div>{`data: ${JSON.stringify(params)}`}</div>
       <br />
       <div>{`data: ${JSON.stringify(search)}`}</div>
       <Client />
-    </>
+    </Suspense>
   );
 };
 
